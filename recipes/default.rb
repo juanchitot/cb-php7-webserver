3978c7beb76905c9f363f0229af69519b211d4c4
juanchitot <juanchitot@gmail.com>
2016-01-28 17:20:35 -0300

    Added ubuntu only code

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,9 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-#p node.default
-# {"apt"=>{"cacher-client"=>{"restrict_environment"=>false}, "cacher_dir"=>"/var/cache/apt-cacher-ng", "cacher_interface"=>nil, "cacher_port"=>3142, "cacher_ssl_support"=>false, "caching_server"=>false, "compiletime"=>false, "compile_time_update"=>false, "key_proxy"=>"", "cache_bypass"=>{}, "periodic_update_min_delay"=>86400, "launchpad_api_version"=>"1.0", "unattended_upgrades"=>{"enable"=>false, "update_package_lists"=>true, "allowed_origins"=>["Ubuntu trusty"], "package_blacklist"=>[], "auto_fix_interrupted_dpkg"=>false, "minimal_steps"=>false, "install_on_shutdown"=>false, "mail"=>nil, "mail_only_on_error"=>true, "remove_unused_dependencies"=>false, "automatic_reboot"=>false, "automatic_reboot_time"=>"now", "dl_limit"=>nil}, "confd"=>{"install_recommends"=>true, "install_suggests"=>false}}, "php7_webserver"=>{"miattrib"=>"juancho"}}
-p node
+
+require 'yaml'
+
+File.write('/tmp/node.json', YAML::dump(node))
 
 include_recipe 'php7_webserver::repository'
 include_recipe 'php7_webserver::php7'
--- recipes/php7.rb
+++ recipes/php7.rb
@@ -1,10 +1,16 @@
-["php7.0", \
- "php7.0-json",\
- "php7.0-mysql", \
- "php7.0-xmlrpc", \
- "php7.0-mcrypt", \
- "php7.0-gd", \
- "php7.0-curl",  \
- "php7.0-cli"].each do |p|
-  package p
+if node['platform'] == 'ubuntu' and
+    package_names = ["php7.0", \
+                     "php7.0-json",\
+                     "php7.0-mysql", \
+                     "php7.0-xmlrpc", \
+                     "php7.0-mcrypt", \
+                     "php7.0-gd", \
+                     "php7.0-curl",  \
+                     "php7.0-cli"]
+end
+
+if package_names.size > 0
+  package_names.each do |p|
+    package p
+  end 
 end
